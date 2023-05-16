f9db56e7d3bc5d0b072b2421dd33c524a0e9d0c5
Tushar Kawade <tusharka@synerzipune.local>
2023-05-16 23:11:03 +0530

    first commit

---
+++ src/Hooks/useStore.tsx
@@ -0,0 +1,4 @@
+import { useContext } from "react";
+import { rootStoreContext } from "../Store/Index";
+
+export const useStore = () => useContext(rootStoreContext);
---
+++ src/Store/CountStore.ts
@@ -0,0 +1,30 @@
+import { IRootStore } from "./RootStore";
+import { action, computed, makeObservable, observable } from "mobx";
+
+export class CountStore {
+  count: number = 1;
+  rootStore: IRootStore;
+
+  constructor(rootStore: IRootStore) {
+    makeObservable(this, {
+      count: observable,
+      increment: action,
+      decrement: action,
+      getCountValue: computed,
+    });
+    this.rootStore = rootStore;
+  }
+
+  increment = () => {
+    this.count++;
+  };
+
+  decrement = () => {
+    // console.log(this.rootStore.githubUserDetails);
+    this.count--;
+  };
+
+  get getCountValue() {
+    return this.count;
+  }
+}
